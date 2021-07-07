b3183c64674a85abae8dc1e09b8f0ea49c247a01
Dennyson Oliveira <dennyson4@hotmail.com>
2021-07-06 22:32:06 -0300

    add 3

---
+++ classe_objt.php
@@ -0,0 +1,32 @@
+<h2>OBJETOS</h2>
+<hr>
+<small> Classes e objetos </small>
+
+<h3>Propriedades</h3>
+<?php>
+
+class carro{
+     public $ano;
+     public $modelo;
+     public $placa;
+     public $cpf_dono;
+}
+
+$carro1 = new carro();
+
+<?>
+
+<h4>Propriedades do Objeto carro </h4>
+<pre><?php print_r($carro1);  ?></pre>
+
+<h3>Métodos</h3>
+
+<h4>Dono do Carro1</h4>
+<p></p>
+<br>
+
+<h4>--------- Troca do dono do carro ---------</h4>
+
+<br>
+
+<h4> Novo Dono do Carro1: </h4>
