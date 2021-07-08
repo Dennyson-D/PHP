315a1b09b772617bf4ad71a95605e04b18890b1f
Dennyson Oliveira <dennyson4@hotmail.com>
2021-07-07 23:11:34 -0300

    Update tipo-objetos.php

--- curso-php/tipo-objetos.php
+++ curso-php/tipo-objetos.php
@@ -20,9 +20,22 @@
 			 public $modelo;
 			 public $placa;
 			 public $cpf_dono;
-		}
 
-		$carro1 = new carro();
+			 # usando construct
+
+			function __construct($ano,$modelo,$placa,$cpf)
+			{
+				$this->ano  = $ano;
+				$this->modelo = $modelo;
+				$this->placa = $placa;
+				$this->cpf_dono = $cpf;
+			}
+		}
+        #   exemplo de como inserir valor na propriedade do objeto manualmente
+		#$carro1 -> ano = 2013;
+
+		#construct
+		$carro1 = new carro(2013,'Classic','MK1144',485555);
 
 		?>
 		<h4>Propriedades do Objeto Carro1: </h4>
