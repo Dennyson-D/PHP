c1a2aae0cb2f96901449fa0846456c0c0b429bb6
Dennyson Oliveira <dennyson4@hotmail.com>
2021-07-12 22:16:01 -0300

    Update tipo-objetos.php

--- curso-php/tipo-objetos.php
+++ curso-php/tipo-objetos.php
@@ -16,10 +16,10 @@
 		<?php
 
 		class carro{
-			 public $ano;
-			 public $modelo;
-			 public $placa;
-			 public $cpf_dono;
+			 private $ano;
+			 private $modelo;
+			 private $placa;
+			 private $cpf_dono;
 
 			 # usando construct
 
@@ -30,29 +30,46 @@
 				$this->placa = $placa;
 				$this->cpf_dono = $cpf;
 			}
+
+			public function mostrar_info()
+			{
+				echo 'Ano: ' . $this->ano . '<br>';
+				echo 'Modelo: ' . $this->modelo . '<br>';
+				echo 'Placa: ' .  $this->placa . '<br>';
+			}
+
+			public function mostrar_cpf()
+			{
+				echo 'CPF: ' . $this->cpf_dono;
+			}
+
+			public function mudar_dono($cpf)
+			{
+			    $this->cpf_dono = $cpf;		
+			}
 		}
         #   exemplo de como inserir valor na propriedade do objeto manualmente
 		#$carro1 -> ano = 2013;
 
 		#construct
-		$carro1 = new carro(2013,'Classic','MK1144',485555);
+		$carro1 = new carro(2013,'Classic','MK1144','485555');
 
 		?>
 		<h4>Propriedades do Objeto Carro1: </h4>
-		<pre><?php print_r($carro1);  ?></pre>
+		<pre><?php $carro1->mostrar_info();  ?></pre>
 
 		<h3>Métodos</h3>
 
 		<h4>Dono do Carro1: </h4>
-		<p></p>
+		<p><?php $carro1->mostrar_cpf();    ?></p>
 		<br>
 		
 		<h4>---------- Troca do dono do carro ----------</h4>
 
 		<br>
-
+		<?php $carro1->mudar_dono('777777'); ?>
 		<h4>Novo Dono do Carro1: </h4>
-		<p></p>
+		<p><?php $carro1->mostrar_cpf();  ?></p>
 
 
 
